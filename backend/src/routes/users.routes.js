1f24794b093b43bcf74860981c03b84882a7334e
Maria Vitoria <maria_vb_alves@estudante.sesisenai.org.br>
2026-03-11 17:02:39 -0300

    rotasApiDog

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const { me } = require("../controllers/users.controller");
+const { me, updateUsuario } = require("../controllers/users.controller");
 const { authRequired } = require("../middlewares/auth.middleware");
 const router = express.Router();
 
 router.get("/me", authRequired, me);
 
+router.put("/:id", updateUsuario);
+
 module.exports = router;
+
